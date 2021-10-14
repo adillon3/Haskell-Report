d7694949a95a05a8233e524722bc10e77c7918c5
adillon3 <adillon@chapman.edu>
2021-10-13 20:50:08 -0700

    corrected if statement on line 50 and added return to main

--- RomanNumerals.hs
+++ RomanNumerals.hs
@@ -47,7 +47,7 @@ arabic_to_roman num = if (n < 0)
                                   then (X(arabic_to_roman (n-10)))
                                   else if (n > 5)
                                     then (V(arabic_to_roman (n-5)))
-                                    if (n > 1)
+                                    else if (n > 1)
                                       then (I(arabic_to_roman (n-1)))
                                       else (error "Roman Numeral could not be found")
 
@@ -58,3 +58,5 @@ arabic_to_roman num = if (n < 0)
 ----------
 
 main = do
+
+  return ()
