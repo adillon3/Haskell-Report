952aec51225b3d471666fc51467730cbec88eb6a
adillon3 <adillon@chapman.edu>
2021-10-15 14:56:04 -0700

    Cleaned up code and created better labels

--- RomanNumerals.hs
+++ RomanNumerals.hs
@@ -1,7 +1,9 @@
 import Data.List
 import System.IO
 
---ROMAN_TO_ARRAIC
+-------------------
+--ROMAN_TO_ARRAIC--
+-------------------
 roman_to_arabic :: String -> Integer
 roman_to_arabic x = convertIntegers(getCharacters x)
 
@@ -29,7 +31,9 @@ getValueOfCharacter 'C' = 100
 getValueOfCharacter 'D' = 500
 getValueOfCharacter 'M' = 1000
 
---ARRABIC_TO_ROMAN
+--------------------
+--ARRABIC_TO_ROMAN--
+--------------------
 arabic_to_roman :: Integer -> String
 arabic_to_roman 0 = []
 arabic_to_roman num = if (num > 0)
@@ -49,40 +53,6 @@ arabic_to_roman num = if (num > 0)
                                         then ("I" ++ (arabic_to_roman (num-1)))
                                         else (error "Roman Numerals must be positive numbers greater than zero")
                                         else (error "Roman Numerals must be positive numbers greater than zeroII")
-
-
-
-
-
---Converting arabic numerals/integers to roman numerals
--- arabic_to_roman :: Integer -> ROMAN
--- arabic_to_roman 1 = I
--- arabic_to_roman 5 = V
--- arabic_to_roman 10 = X
--- arabic_to_roman 50 = L
--- arabic_to_roman 100 = C
--- arabic_to_roman 500 = D
--- arabic_to_roman 1000 = M
-
--- arabic_to_roman num = if (num < 0)
---                        then (error "Roman Numerals must be positive numbers greater than zero")
---                        else if (num > 1000)
---                          then (M (arabic_to_roman (num-1000)))
---                          else if (num > 500)
---                            then (D (arabic_to_roman (num-500)))
---                            else if (num > 100)
---                              then (C (arabic_to_roman (num-100)))
---                              else if (num > 100)
---                                then (L (arabic_to_roman (num-50)))
---                                else if (num > 10)
---                                  then (X (arabic_to_roman (num-10)))
---                                  else if (num > 5)
---                                    then (V (arabic_to_roman (num-5)))
---                                    else if (num > 1)
---                                      then (I (arabic_to_roman (num-1)))
---                                      else (error "Roman Numeral could not be found")
-
-
 
 ----------
 -- Testing
