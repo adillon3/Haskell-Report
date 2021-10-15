e1a841d366c33a5707ae5b3f4c53fbe59d3dd53f
adillon3 <adillon@chapman.edu>
2021-10-15 14:54:59 -0700

    Started conversion from Arabic to Roman numberals, though it cannot "Normalize numbers(such as with 4 converting IIII to IV))

--- RomanNumerals.hs
+++ RomanNumerals.hs
@@ -30,15 +30,28 @@ getValueOfCharacter 'D' = 500
 getValueOfCharacter 'M' = 1000
 
 --ARRABIC_TO_ROMAN
+arabic_to_roman :: Integer -> String
+arabic_to_roman 0 = []
+arabic_to_roman num = if (num > 0)
+                        then if (num >= 1000)
+                          then ("M" ++ (arabic_to_roman (num-1000)))
+                          else if (num >= 500)
+                            then ("D" ++ (arabic_to_roman (num-500)))
+                            else if (num >= 100)
+                              then ("C" ++ (arabic_to_roman (num-100)))
+                                else if (num >= 50)
+                                  then ("L" ++ (arabic_to_roman (num-50)))
+                                  else if (num >= 10)
+                                    then ("X" ++ (arabic_to_roman (num-10)))
+                                    else if (num >= 5)
+                                      then ("V" ++ (arabic_to_roman (num-5)))
+                                      else if (num >= 1)
+                                        then ("I" ++ (arabic_to_roman (num-1)))
+                                        else (error "Roman Numerals must be positive numbers greater than zero")
+                                        else (error "Roman Numerals must be positive numbers greater than zeroII")
 
 
--- roman_to_arabic (val I) = (roman_to_arabic val) + 1
--- roman_to_arabic V val = (roman_to_arabic val) + 5
--- roman_to_arabic X val = (roman_to_arabic val) + 10
--- roman_to_arabic L val = (roman_to_arabic val) + 50
--- roman_to_arabic C val = (roman_to_arabic val) + 100
--- roman_to_arabic D val = (roman_to_arabic val) + 500
--- roman_to_arabic M val = (roman_to_arabic val) + 1000
+
 
 
 --Converting arabic numerals/integers to roman numerals
@@ -74,7 +87,6 @@ getValueOfCharacter 'M' = 1000
 ----------
 -- Testing
 ----------
-
 main = do
   -- print $ "** TESTING arabic_to_roman **"
   -- print $ "arabic_to_roman 1 - Expected: I, Actual: " ++ show (arabic_to_roman 1)
@@ -93,15 +105,23 @@ main = do
   print $ "roman_to_arabic C - Expected: 100, Actual: " ++ show (roman_to_arabic "C")
   print $ "roman_to_arabic D - Expected: 500, Actual: " ++ show (roman_to_arabic "D")
   print $ "roman_to_arabic M - Expected: 1000, Actual: " ++ show (roman_to_arabic "M")
-
-
   print $ "roman_to_arabic II - Expected: 2, Actual: " ++ show (roman_to_arabic "II")
-  print $ "roman_to_arabic IX - Expected: 9, Actual: " ++ show (roman_to_arabic "IX")
+  print $ "roman_to_arabic XL - Expected: 40, Actual: " ++ show (roman_to_arabic "XL")
 
 
-  -- let roman = "IX"
-  -- let list = getSymbols roman
-  -- print $ convert list
+
+  print $ "** TESTING roman_to_arabic **"
+  print $ "arabic_to_roman 1 - Expected: I, Actual: " ++ show (arabic_to_roman 1)
+  print $ "arabic_to_roman 5 - Expected: V, Actual: " ++ show (arabic_to_roman 5)
+  print $ "arabic_to_roman 10 - Expected: X, Actual: " ++ show (arabic_to_roman 10)
+  print $ "arabic_to_roman 50 - Expected: L, Actual: " ++ show (arabic_to_roman 50)
+  print $ "arabic_to_roman 100 - Expected: C, Actual: " ++ show (arabic_to_roman 100)
+  print $ "arabic_to_roman 500 - Expected: D, Actual: " ++ show (arabic_to_roman 500)
+  print $ "arabic_to_roman 1000 - Expected: M, Actual: " ++ show (arabic_to_roman 1000)
+
+  print $ "arabic_to_roman 2 - Expected: II, Actual: " ++ show (arabic_to_roman 2)
+  print $ "arabic_to_roman 4 - Expected: IV, Actual: " ++ show (arabic_to_roman 4)
+
 
 
   return ()
